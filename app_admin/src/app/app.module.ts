6cda20110ef0921d7bceaabafbbc07549234b9e4
Aydar Fayzullin <faidarik@mail.ru>
2023-10-10 23:38:34 -0400

    Feat: Create Add and Edit Trip components

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpModule } from "@angular/http";
-
-import { AppComponent } from "./app.component";
-import { TripListingComponent } from "./trip-listing/trip-listing.component";
-import { TripCardComponent } from "./trip-card/trip-card.component";
-import { TripDataService } from "services/trip-data.service";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { TripCardComponent } from './trip-card/trip-card.component';
+import { TripAddComponent } from './trip-add/trip-add.component';
+import { TripDataService } from './services/trip-data.service';
+import { TripEditComponent } from './trip-edit/trip-edit.component';
 
 @NgModule({
-  declarations: [AppComponent, TripListingComponent, TripCardComponent],
-  imports: [BrowserModule, HttpModule],
-  providers: [TripDataService],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    TripListingComponent,
+    TripCardComponent,
+    TripAddComponent,
+    TripEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+    TripDataService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule {}
+export class AppModule { }
--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input } from '@angular/core';
+import { Trip } from '../models/trip';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-trip-card",
-  templateUrl: "./trip-card.component.html",
-  styleUrls: ["./trip-card.component.css"],
+  selector: 'app-trip-card',
+  templateUrl: './trip-card.component.html',
+  styleUrls: ['./trip-card.component.css']
 })
 export class TripCardComponent implements OnInit {
-  @Input("trip") trip: any;
-  constructor() {}
+  @Input('trip') trip!: Trip;
 
-  ngOnInit(): void {}
+  constructor(private router: Router) { }
+
+  ngOnInit(): void { }
+
+  editTrip(trip: Trip): void {
+    console.log('TripCardComponent#editTrip setting tripCode in localStorage', trip.code);
+    localStorage.removeItem('tripCode');
+    localStorage.setItem('tripCode', trip.code);
+    console.log('TripCardComponent#editTrip routing to TripEditComponent');
+    this.router.navigate(['edit-trip']);
+  }
 }
